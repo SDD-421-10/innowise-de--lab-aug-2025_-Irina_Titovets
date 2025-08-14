53568a181b6f4caf6ecbd426bd33f88a3c25ec95
Ирина Титовец <titovets.ira@mail.ru>
2025-08-14 08:49:07 +0300

    Add homework 3

---
+++ homework 3/orders_202508132031.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.orders (order_id,item,amount,customer_id) VALUES
+	 (1,'Keyboard',400,4),
+	 (2,'Mouse',300,4),
+	 (3,'Monitor',12000,3),
+	 (4,'Keyboard',400,1),
+	 (5,'Mousepad',250,2),
+	 (6,'Monitor',10000,6),
+	 (7,'Keyboard',450,6),
+	 (8,'Mouse',350,7),
+	 (9,'Monitor',11000,9),
+	 (10,'Mousepad',300,10);
