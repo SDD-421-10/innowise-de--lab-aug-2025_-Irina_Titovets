a26763ba1c5b18556b570d12c04fe696e46ed162
Ирина Титовец <titovets.ira@mail.ru>
2025-08-18 12:55:17 +0300

    Add homework 4

---
+++ homework 4/projects_202508181100.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.projects (projectname,budget,startdate,enddata) VALUES
+	 ('Website Redesign',165000.00,'2023-01-15','2023-06-30'),
+	 ('Mobile App Development',220000.00,'2023-03-01','2023-10-31'),
+	 ('Internal Tools Upgrade',88000.00,'2023-05-10','2023-09-15'),
+	 ('Logo design',NULL,'2025-08-17','2026-08-17');
