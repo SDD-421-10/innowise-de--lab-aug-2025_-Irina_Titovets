62bb05c3353b348b692b7c9a7f701602e7953aa2
SDD-421-10 <titovets.ira@mail.ru>
2025-08-25 14:03:42 +0300

    Delete homework 4 directory

--- homework 4/employeeprojects_202508181100.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.employeeprojects (employeeid,projectid,hoursworked) VALUES
-	 (2,1,160),
-	 (4,1,120),
-	 (2,2,200),
-	 (1,3,80),
-	 (3,3,100),
-	 (11,1,80);
--- homework 4/employees_202508181100.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.employees (firstname,lastname,department,salary,email) VALUES
-	 ('Charlie','Brown','Finance',62000.00,'table@mail.com'),
-	 ('Leo','DiCaprio',NULL,NULL,NULL),
-	 ('Bob','Johnson','Senior IT',75000.00,'vase@mail.com'),
-	 ('Alice','Smith','Senior IT',71500.00,'flower@mail.com'),
-	 ('Johnny','Depp','Frontend Developer',80000.00,'hansome@mail.com');
