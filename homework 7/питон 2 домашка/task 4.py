1ea45ddeecdaab75924e5c7686dfbace6f3127e6
Ирина Титовец <titovets.ira@mail.ru>
2025-08-28 13:06:54 +0300

    Add homework 7

---
+++ homework 7/питон 2 домашка/task 4.py
@@ -0,0 +1,17 @@
+scores = [75, 88, -10, 95, 100, -25, 89]
+total = 0
+for score in scores:
+    if score < 0:
+        continue
+        # условие если score отрицательный, мы его пропускаем с помощью continue
+    if score == 0:
+        print("Обнаружен 0. Подсчёт прерван.")
+        break
+        # если score 0, то мы прерываем цикл с помощью break
+    total += score
+    print(f"Добавлен балл: {score}")
+else:
+
+    print("Все данные обработаны корректно.")
+
+print(f"Итоговая сумма баллов:{total}")
