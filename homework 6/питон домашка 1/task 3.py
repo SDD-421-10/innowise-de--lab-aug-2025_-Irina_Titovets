d92c54a0e23124bf59f5cc50919b4a6c635f429d
SDD-421-10 <titovets.ira@mail.ru>
2025-08-31 20:46:00 +0300

    Delete homework 6 directory

--- homework 6/питон домашка 1/task 3.py
+++
@@ -1,8 +0,0 @@
-# Напишите программу, которая запрашивает у
-# пользователя температуру в градусах Цельсия, переводит её в градусы
-# Фаренгейта и выводит результат на экран.
-temperature_c = float(input("Введите температуру в градусах Цельсия: "))
-temperature_f = (temperature_c * 9 / 5) + 32
-temperature_c_str = str(temperature_c)
-temperature_f_str = str(temperature_f)
-print(f"{temperature_f} - это {temperature_c}")
