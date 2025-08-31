1b7057c495a09e44d15454f46bda0ea54deacb0d
SDD-421-10 <titovets.ira@mail.ru>
2025-08-31 20:48:35 +0300

    Delete homework 7 directory

--- homework 7/питон 2 домашка/task 1.py
+++
@@ -1,7 +0,0 @@
-shopping_list = ["milk", "bread", "eggs", "butter", "apples"]
-print("Список покупок:")
-for i in range (5):
-    # использовала функцию range() в цикле for, чтобы указать нумерацию
-    print(f"{i + 1}. {shopping_list[i]}")
-# прописала i+1 т к нумерация начинается с нуля
-# в print использовала f (формативную строку)
